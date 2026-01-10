3b00b7f7740ff652acf32f7e7347d1651abadb0d
ferrypradana07 <ferrykun500@gmail.com>
2026-01-10 11:27:33 +0700

    refactr: change line

--- app/(landing)/components/ui/button.tsx
+++ app/(landing)/components/ui/button.tsx
@@ -6,18 +6,18 @@ type TButtonProps = {
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({ children, className, variant="primary", size="normal", ...props}: TButtonProps) => {
-    const baseStyles = "inline-flex items-center justify-center duration-300 cursor-pointer gap-2 hover:scale-105"
+    const baseStyles = "inline-flex items-center justify-center duration-300 cursor-pointer gap-2 hover:scale-105";
     
     const variants = {
         primary: "bg-primary text-white bg-primary/85",
         dark: "bg-dark text-white bg-dark/85",
         ghost: "bg-transparent text-dark hover:bg-gray/100",
-    }
+    };
 
     const sizes = {
         normal: "px-9 py-4 ",
         small: "=x=7 py-[10px] ",
-    }
+    };
     
     return (
         <button className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`} {...props}>
