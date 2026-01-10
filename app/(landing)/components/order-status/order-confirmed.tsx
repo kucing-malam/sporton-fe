1005fdfa41037f59dba32256782ac305f247f86c
ferrypradana07 <ferrykun500@gmail.com>
2026-01-10 11:26:12 +0700

    feat: add order status components

---
+++ app/(landing)/components/order-status/order-confirmed.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import Image from "next/image";
+
+const OrderConfirmed = () => {
+
+    return (
+        <div className="bg-white w-160 p-15.5 flex flex-col justify-center items-center ">
+            <Image src="/images/icon-order-confirmed.svg" alt="icon order submitted"
+                width={117}
+                height={117}
+                className="mb-4"
+            />
+            <h2 className="text-xl font-semibold mb-2">Order Confirmed!!</h2>
+            <p className="text-center mb-8">We have received your payment, and your order is
+                currently processed by our staff, just wait until your favorite sportswear arrive in your home.
+                We will contact you in Whatsapp for further shipping updates.
+            </p>
+        </div>
+    )
+}
+
+export default OrderConfirmed;
