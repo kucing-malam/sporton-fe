d2b8387a125a4e30b4be1c87cc639b330ef46881
ferrypradana07 <ferrykun500@gmail.com>
2026-01-13 21:48:47 +0700

    feat: update OrderStatus component to fetch transaction data and display status dynamically

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -1,17 +1,21 @@
-'use client'
-
-import { useState } from "react"
-import OrderSubmitted from "../../components/order-status/order-submitted"
+import OrderSubmitted from "../../components/order-status/order-submitted";
 import OrderConfirmed from "../../components/order-status/order-confirmed";
+import { getTransactionByID } from "@/app/services/transaction.service";
+import { TPageProps } from "../../product/[id]/page";
+import OrderRejected from "../../components/order-status/order-rejected";
 
-const OrderStatus = () => {
-    const [isConfirmed, setIsConfirmed] = useState(true);
+const OrderStatus = async ({params}: TPageProps) => {
+    const {id} = await params;
+    const transaction = await getTransactionByID(id);
+
     return (
         <main className="bg-gray-100 min-h-[80vh]">
-            <div className="max-w-5xl mx-auto py-20">
+            <div className="max-w-5xl mx-auto py-20 pt-30">
                 <h1 className="font-bold text-5xl text-center mb-11">Order Status</h1>
                 <div className="flex justify-center">
-                    {isConfirmed ? (<OrderSubmitted />) : (<OrderConfirmed />)}
+                    {transaction.status === "paid" && (<OrderConfirmed />) }
+                    {transaction.status === "pending" && (<OrderSubmitted />) }
+                    {transaction.status === "reject" && (<OrderRejected />) }
                 </div>
             </div>
         </main>
