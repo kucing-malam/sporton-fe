6622e1562a99b16e980283349d77132e438a61b8
ferrypradana07 <ferrykun500@gmail.com>
2026-01-13 21:46:35 +0700

    feat: enhance OrderInformation component with form data handling and input binding

--- app/(landing)/components/checkout/order-information.tsx
+++ app/(landing)/components/checkout/order-information.tsx
@@ -1,20 +1,52 @@
+"use client";
+
+import { CustomerInfo } from "@/app/hook/use-cart-store";
 import CardWithHeader from "../ui/card-with-header";
 
-const OrderInformation = () => {
+type TOrderInfoProps = {
+    formData: CustomerInfo;
+    setFormData: React.Dispatch<React.SetStateAction<CustomerInfo>>
+};
+const OrderInformation = ({ formData, setFormData }: TOrderInfoProps) => {
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    };
     return (
         <CardWithHeader title="Order Information">
             <div className="p-5">
                 <div className="input-group ">
-                    <label htmlFor="full_name">Full Name</label>
-                    <input type="text" placeholder="Type your full name" id="full_name" />
+                    <label htmlFor="customerName">Full Name</label>
+                    <input
+                        value={formData.customerName}
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="Type your full name"
+                        id="customerName"
+                        name="customerName"
+                    />
                 </div>
                 <div className="input-group ">
-                    <label htmlFor="wa_number">Whatsapp Number</label>
-                    <input type="text" placeholder="Type your whatsapp number" id="wa_number" />
+                    <label htmlFor="customerContact">Whatsapp Number</label>
+                    <input
+                        value={formData.customerContact ?? ""}
+                        onChange={handleInputChange}
+                        type="number"
+                        placeholder="Type your whatsapp number"
+                        id="customerContact"
+                        name="customerContact"
+                    />
                 </div>
                 <div className="input-group ">
-                    <label htmlFor="shipping_address">Shipping Address</label>
-                    <textarea rows={7} placeholder="Type your shipping address" id="shipping_address" />
+                    <label htmlFor="customerAddress">Shipping Address</label>
+                    <textarea
+                        value={formData.customerAddress}
+                        onChange={handleInputChange}
+                        rows={7}
+                        placeholder="Type your shipping address"
+                        id="customerAddress"
+                        name="customerAddress"
+                    />
                 </div>
             </div>
         </CardWithHeader>
