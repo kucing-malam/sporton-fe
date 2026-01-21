7a963782716f9885272f10d6b8bdcdf941126021
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 23:03:13 +0700

    feat: create service for product

--- app/services/product.service.ts
+++ app/services/product.service.ts
@@ -1,4 +1,4 @@
-import { fetchAPI } from '../lib/api'
+import { fetchAPI, getAuthHeader } from '../lib/api'
 import { Product } from '../types/index'
 
 export const getAllProducts = async (): Promise<Product[]> => {
@@ -7,4 +7,33 @@ export const getAllProducts = async (): Promise<Product[]> => {
 
 export const getProductDetail = async (id: string): Promise<Product> => {
     return await fetchAPI<Product>(`/products/${id}`);
+}
+
+export const createProduct = async (data: FormData): Promise<Product> => {
+    return await fetchAPI<Product>('/products', {
+        body: data,
+        headers: {
+            ...getAuthHeader()
+        },
+        method: "POST"
+    });
+}
+
+export const updateProduct = async (id: string, data: FormData): Promise<Product> => {
+    return await fetchAPI<Product>(`/products/${id}`, {
+        body: data,
+        headers: {
+            ...getAuthHeader()
+        },
+        method: "PUT"
+    });
+}
+
+export const deleteProduct = async (id: string): Promise<void> => {
+    return await fetchAPI<void>(`/products/${id}`, {
+        headers: {
+            ...getAuthHeader()
+        },
+        method: "DELETE"
+    });
 }
