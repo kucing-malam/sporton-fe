778f8e3dda4158917fe5f349403c68b8b095e0b4
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 23:01:18 +0700

    fear: implement API

--- app/(dashboard)/component/layouts/category/category-modal.tsx
+++ app/(dashboard)/component/layouts/category/category-modal.tsx
@@ -1,20 +1,92 @@
 import Button from "@/app/(landing)/components/ui/button";
 import Modal from "../../ui/modal";
 import ImageUploadPreview from "../../ui/image-upload-preview";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Category } from "@/app/types";
+import { createCategory, updateCategory } from "@/app/services/category.service";
+import { getImageUrl } from "@/app/lib/api";
+import { toast } from "react-toastify";
 
 type TCategoryModalProps = {
     isOpen: boolean;
     onClose: () => void;
+    category: Category | null;
+    onSuccess: () => void;
 };
 
-const CategoryModal = ({ isOpen, onClose }: TCategoryModalProps) => {
+type categoryFormData = {
+    name: string;
+    description: string;
+};
+
+const CategoryModal = ({ isOpen, onClose, category, onSuccess }: TCategoryModalProps) => {
     const [imageFile, setImageFile] = useState<File | null>(null);
     const [imagePreview, setImagePreview] = useState<string | null>(null);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [formData, setFormData] = useState<categoryFormData>({
+        name: "",
+        description: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { id, value } = e.target;
+        setFormData((prev) => ({ ...prev, [id]: value }));
+    };
+
+    const isEditMode = !!category;
+    const handleSubmit = async () => {
+        setIsSubmitting(true);
+        try {
+            const data = new FormData;
+            data.append("name", formData.name);
+            data.append("description", formData.description);
+            if (imageFile) {
+                data.append("image", imageFile);
+            }
+            if (category) {
+                await updateCategory(category._id, data);
+            } else {
+                await createCategory(data);
+            }
+            toast.success(isEditMode ? "Category updated successfully" : "Category create successfully");
+
+            // Reset FormData
+            setFormData({
+                name: "",
+                description: ""
+            });
+            setImageFile(null);
+            setImagePreview(null);
+
+            onSuccess?.();
+            onClose?.();
+        } catch (error) {
+            console.error(isEditMode ? "failed to update category" : "failed to create category");
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    useEffect(() => {
+        if (isOpen && isEditMode) {
+            setFormData({
+                name: category.name,
+                description: category.description
+            });
+            setImagePreview(category.imageUrl ? getImageUrl(category.imageUrl) : null);
+        } else if (isOpen) {
+            setFormData({
+                name: "",
+                description: ""
+            });
+            setImageFile(null);
+            setImagePreview(null);
+        }
+    }, [isOpen, category]);
 
     return (
-        <Modal isOpen={isOpen} onClose={onClose} title="Add New Category">
-            <div className="flex flex-col gap-6">
+        <Modal isOpen={isOpen} onClose={onClose} title={isEditMode ? "Edit category" : "Add Category"}>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-6">
                 <div className="flex gap-7">
                     <div className="min-w-50 aspect-square">
                         <ImageUploadPreview
@@ -27,17 +99,42 @@ const CategoryModal = ({ isOpen, onClose }: TCategoryModalProps) => {
                     </div>
                     <div className="flex-col flex gap-4 w-full input-group-admin">
                         <div>
-                            <label htmlFor="categoryName">Category Name</label>
-                            <input type="text" name="categoryName" id="categoryName" placeholder="e. g. Running" />
+                            <label htmlFor="name">Category Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                value={formData.name}
+                                onChange={handleChange}
+                                placeholder="e. g. Running"
+                            />
                         </div>
                         <div>
                             <label htmlFor="description">Description</label>
-                            <textarea name="description" className="min-h-11" rows={4} id="description" placeholder="Category Detail" ></textarea>
+                            <textarea
+                                name="description"
+                                className="min-h-11"
+                                rows={4}
+                                id="description"
+                                value={formData.description}
+                                onChange={handleChange}
+                                placeholder="Category Detail"
+                                disabled={isSubmitting}
+                            ></textarea>
                         </div>
                     </div>
                 </div>
-                <Button className="px-4 mt-3 ml-auto rounded-lg" variant="primary" size="small">Create Category</Button>
-            </div>
+                <Button
+                    type="submit"
+                    className="px-4 mt-3 ml-auto rounded-lg"
+                    variant="primary"
+                    size="small"
+                >
+                    {
+                        isEditMode ? "Update category" : "Create Category"
+                    }
+                </Button>
+            </form>
         </Modal>
     )
 }
--- app/(dashboard)/component/layouts/category/category-table.tsx
+++ app/(dashboard)/component/layouts/category/category-table.tsx
@@ -1,37 +1,18 @@
 "use client";
 
+import { getImageUrl } from "@/app/lib/api";
+import { Category } from "@/app/types";
 import Image from "next/image";
 import { FiEdit2, FiTrash2 } from "react-icons/fi";
 
+type TCategoryTableProps = {
+    categories: Category[];
+    onEdit? : (category: Category) => void; 
+    onDelete? : (id: string) => void; 
+};
 
-const CategoryTable = () => {
-    const datas = [
-        {
-            name: "Badminton",
-            description: 'Badminton is category',
-            imageUrl: "/images/categories/category-badminton.png"
-        },
-        {
-            name: "Running",
-            description: 'Running is category',
-            imageUrl: "/images/categories/category-running.png"
-        },
-        {
-            name: "Basketball",
-            description: 'Basketball is category',
-            imageUrl: "/images/categories/category-basketball.png"
-        },
-        {
-            name: "Swimming",
-            description: 'Swimming is category',
-            imageUrl: "/images/categories/category-swimming.png"
-        },
-        {
-            name: "Football",
-            description: 'Football is category',
-            imageUrl: "/images/categories/category-football.png"
-        }
-    ]
+const CategoryTable = ({categories, onEdit, onDelete}: TCategoryTableProps) => {
+
     return (
         <div className="bg-white rounded-xl border border-gray-200">
             <table className="w-full text-left border-collapse">
@@ -44,30 +25,35 @@ const CategoryTable = () => {
                 </thead>
                 <tbody>
                     {
-                        datas.map((category, index) => (
-                            <tr key={index} className="border-b border-gray-200 last:border-b-0">
+                        categories.map((category) => (
+                            <tr key={category._id} className="border-b border-gray-200 last:border-b-0">
                                 <td className="px-6 py-4 font-medium">
                                     <div className="flex gap-2 items-center">
                                         <div className="aspect-square bg-gray-100 rounded-md ">
                                             <Image
-                                                src={category.imageUrl}
+                                                src={getImageUrl(category.imageUrl)}
                                                 alt={category.name}
                                                 height="52"
                                                 width="52"
                                                 className="aspect-square object-contain"
-                                            ></Image>
+                                                unoptimized={true}
+                                             ></ Image>
                                         </div>
                                         <span>{category.name}</span>
                                     </div>
                                 </td>
-                                <td className="px-6 py-4 font-medium">{category.description} Units</td>
+                                <td className="px-6 py-4 font-medium">{category.description}</td>
                                 <td className="px-6 py-8 items-center flex h-full">
                                     <div className="flex gap-3">
-                                        <button>
-                                            <FiEdit2 className="cursor-pointer" size={20} />
+                                        <button
+                                            className="cursor-pointer"
+                                            onClick={() => onEdit?.(category)}>
+                                            <FiEdit2 size={20} />
                                         </button>
-                                        <button>
-                                            <FiTrash2 className="cursor-pointer" size={20} />
+                                        <button
+                                            className="cursor-pointer"
+                                            onClick={() => onDelete?.(category._id)}>
+                                            <FiTrash2 size={20} />
                                         </button>
                                     </div>
                                 </td>
