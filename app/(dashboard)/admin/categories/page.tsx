67e0b85be2403efeca60a98286d73dde80e2a63c
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 23:05:02 +0700

    feat: implement service for categorymanagement

--- app/(dashboard)/admin/categories/page.tsx
+++ app/(dashboard)/admin/categories/page.tsx
@@ -4,14 +4,59 @@ import Button from "@/app/(landing)/components/ui/button";
 import { FiPlus } from "react-icons/fi";
 import CategoryTable from "../../component/layouts/category/category-table";
 import CategoryModal from "../../component/layouts/category/category-modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Category } from "@/app/types";
+import { deleteCategory, getAllCategories } from "@/app/services/category.service";
+import DeleteModal from "../../component/ui/delete-modal";
+import { toast } from "react-toastify";
 
 const CategoryManagement = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleClose = () => {
-        setIsOpen(!isOpen);
-    }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+    const [categoryToDeleteId, setCategoryToDeleteId] = useState("");
 
+    const fetchCategories = async () => {
+        try {
+            const data = await getAllCategories();
+            setCategories(data);
+        } catch (error) {
+            console.error("failed to fetch categories")
+        }
+    };
+
+    const handleEdit = (category: Category) => {
+        setSelectedCategory(category);
+        setIsModalOpen(!isModalOpen);
+    };
+
+    const handleDelete = (id: string) => {
+        setCategoryToDeleteId(id);
+        setIsDeleteModalOpen(!isDeleteModalOpen)
+    };
+
+    const handleDeleteConfirm = async () => {
+        if (!isDeleteModalOpen) return;
+        try {
+            await deleteCategory(categoryToDeleteId);
+            fetchCategories();
+            toast.success("category deleted successfully");
+        } catch (error) {
+            console.error("failed to delete category ", error);
+            toast.error("failed to delete category");
+        }
+        setIsDeleteModalOpen(!isDeleteModalOpen);
+    };
+
+    const handleClose = () => {
+        setSelectedCategory(null);
+        setIsModalOpen(!isModalOpen);
+    };
+
+    useEffect(() => {
+        fetchCategories();
+    });
     return (
         <div>
             <div className="flex justify-between items-center mb-10">
@@ -28,8 +73,22 @@ const CategoryManagement = () => {
                     </Button>
                 </div>
             </div>
-            <CategoryTable />
-            <CategoryModal isOpen={isOpen} onClose={handleClose}/>
+            <CategoryTable
+                categories={categories}
+                onEdit={handleEdit}
+                onDelete={handleDelete}
+            />
+            <CategoryModal
+                category={selectedCategory}
+                isOpen={isModalOpen}
+                onClose={handleClose}
+                onSuccess={fetchCategories}
+            />
+            <DeleteModal
+                isOpen={isDeleteModalOpen}
+                onClose={() => setIsDeleteModalOpen(false)}
+                onConfirm={handleDeleteConfirm}
+            />
         </div>
     )
 }
