3361ff9386ab1fdc2b8a98f9462743fc36abe187
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 23:01:53 +0700

    feat: implement API

--- app/(dashboard)/component/layouts/products/product-modal.tsx
+++ app/(dashboard)/component/layouts/products/product-modal.tsx
@@ -1,20 +1,133 @@
 import Button from "@/app/(landing)/components/ui/button";
 import Modal from "../../ui/modal"
 import ImageUploadPreview from "../../ui/image-upload-preview";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Category, Product } from "@/app/types";
+import { getAllCategories } from "@/app/services/category.service";
+import { createProduct, updateProduct } from "@/app/services/product.service";
+import { getImageUrl } from "@/app/lib/api";
+import { toast } from "react-toastify";
 
 type TProductModalProps = {
     isOpen: boolean;
+    product?: Product | null;
     onClose: () => void;
+    onSuccess?: () => void;
 };
 
-const ProductModal = ({ isOpen, onClose }: TProductModalProps) => {
+type ProductFormData = {
+    name: string;
+    description: string;
+    categoryId: string;
+    stock: number;
+    price: number;
+}
+
+const ProductModal = ({ isOpen, onClose, product, onSuccess }: TProductModalProps) => {
     const [imageFile, setImageFile] = useState<File | null>(null);
     const [imagePreview, setImagePreview] = useState<string | null>(null);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [isSubmiting, setIsSubmiting] = useState(false);
+
+    const [formData, setFormData] = useState<ProductFormData>({
+        name: "",
+        description: "",
+        categoryId: "",
+        stock: 0,
+        price: 0
+    })
+
+    const isEditMode = !!product;
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsSubmiting(true);
+        try {
+            const data = new FormData;
+            data.append("name", formData.name);
+            data.append("description", formData.description);
+            data.append("categoryId", formData.categoryId);
+            data.append("stock", formData.stock.toString());
+            data.append("price", formData.price.toString());
+            if (imageFile) {
+                data.append("image", imageFile);
+            }
+            if (product) {
+                await updateProduct(product._id, data);
+            } else {
+                await createProduct(data);
+            }
+
+            // Reset FormData
+            setFormData({
+                name: "",
+                description: "",
+                categoryId: "",
+                stock: 0,
+                price: 0
+            });
+            setImageFile(null);
+            setImagePreview(null);
+
+            toast.success(isEditMode ? "Product updated successfully" : "Product created successfully");
+
+            onSuccess?.();
+            onClose?.();
+        } catch (error) {
+            console.error(isEditMode ? "failed to update product" : "failed to create product");
+            toast.error(isEditMode ? "failed to update product" : "failed to create product");
+        } finally {
+            setIsSubmiting(false);
+        }
+    }
+
+    const fetchCategories = async () => {
+        try {
+            const data = await getAllCategories();
+            if (data) {
+                setCategories(data);
+            }
+        } catch (error) {
+            console.error("failed to fetch categories ", error);
+        }
+    }
+
+    useEffect(() => {
+        if (isOpen && isEditMode) {
+            setFormData({
+                name: product.name,
+                description: product.description,
+                categoryId: product.category._id,
+                stock: product.stock,
+                price: product.price,
+            })
+            setImagePreview(product.imageUrl ? getImageUrl(product.imageUrl) : null)
+        } else if (isOpen) {
+            setFormData({
+                name: "",
+                description: "",
+                categoryId: "",
+                stock: 0,
+                price: 0
+            });
+            setImageFile(null);
+            setImagePreview(null);
+        }
+
+    }, [isOpen, product]);
+
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const { id, value } = e.target
+        setFormData((prev) => ({ ...prev, [id]: value }));
+    };
 
     return (
-        <Modal isOpen={isOpen} onClose={onClose} title="Title">
-            <div className="flex flex-col gap-6">
+        <Modal isOpen={isOpen} onClose={onClose} title={isEditMode ? "Edit Product" : "Add Product"}>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-6">
                 <div className="flex gap-7">
                     <div className="min-w-50 aspect-square">
                         <ImageUploadPreview
@@ -22,38 +135,83 @@ const ProductModal = ({ isOpen, onClose }: TProductModalProps) => {
                             onChange={(file) => {
                                 setImageFile(file)
                                 setImagePreview(URL.createObjectURL(file))
-                            }} 
+                            }}
                             value={imagePreview} />
                     </div>
                     <div className="flex-col flex gap-4 w-full input-group-admin">
                         <div>
-                            <label htmlFor="productName">Product Name</label>
-                            <input type="text" name="productName" id="productName" placeholder="e. g. Product Name" />
+                            <label htmlFor="name">Product Name</label>
+                            <input
+                                value={formData.name}
+                                onChange={handleChange}
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="e. g. Product Name"
+                            />
                         </div>
                         <div className="grid grid-cols-2 gap-4">
                             <div>
                                 <label htmlFor="price">Price (IDR)</label>
-                                <input type="number" name="price" id="price" placeholder="0" />
+                                <input
+                                    value={formData.price}
+                                    onChange={handleChange}
+                                    type="number"
+                                    name="price"
+                                    id="price"
+                                    placeholder="1000"
+                                />
                             </div>
                             <div>
                                 <label htmlFor="stock">Stock</label>
-                                <input type="number" name="stock" id="stock" placeholder="0" />
+                                <input
+                                    value={formData.stock}
+                                    onChange={handleChange}
+                                    type="number"
+                                    name="stock"
+                                    id="stock"
+                                    placeholder="1000"
+                                />
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="category">Category</label>
-                            <select name="category" id="category" >
-                                <option value="Running">Running</option>
+                            <label htmlFor="categoryId">Category</label>
+                            <select name="categoryId" id="categoryId" onChange={handleChange}>
+                                <option value="" disabled>Select Catagory</option>
+                                {
+                                    categories.map((category) => (
+                                        <option key={category._id} value={category._id}>{category.name}</option>
+                                    ))
+                                }
                             </select>
                         </div>
                     </div>
                 </div>
                 <div className="flex-col flex input-group-admin">
                     <label htmlFor="description">Description</label>
-                    <textarea name="description" className="min-h-11" rows={5} id="description" placeholder="Product detail" ></textarea>
+                    <textarea
+                        name="description"
+                        className="min-h-11"
+                        rows={5} 
+                        id="description"
+                        placeholder="Product detail"
+                        value={formData.description}
+                        onChange={handleChange}
+                    >
+                    </textarea>
                 </div>
-                <Button className="px-4 mt-3 ml-auto rounded-lg" variant="primary" size="small">Create Product</Button>
-            </div>
+                <Button
+                    className="px-4 mt-3 ml-auto rounded-lg"
+                    variant="primary"
+                    type="submit"
+                    disabled={isSubmiting}
+                    size="small"
+                >
+                    {
+                        isEditMode ? "Update Product" : "Create Product"
+                    }
+                </Button>
+            </form>
         </Modal>
     )
 }
--- app/(dashboard)/component/layouts/products/product-table.tsx
+++ app/(dashboard)/component/layouts/products/product-table.tsx
@@ -1,47 +1,18 @@
 "use client";
+import { getImageUrl } from "@/app/lib/api";
+import { Product } from "@/app/types";
 import PriceFormatter from "@/app/utils/price-formatter";
 import Image from "next/image";
 import { FiEdit2, FiTrash2 } from "react-icons/fi";
 
+type TProductTableProps = {
+    products: Product[];
+    onDelete?: (id: string) => void;
+    onEdit?: (product: Product) => void;
+}
 
-const ProductTable = () => {
-    const datas = [
-        {
-            name: "SportOn Product 1",
-            imageUrl: "/images/products/product-1.png",
-            category: "Running",
-            price: 2000,
-            stock: 20
-        },
-        {
-            name: "SportOn Product 2",
-            imageUrl: "/images/products/product-2.png",
-            category: "Running",
-            price: 2000,
-            stock: 20
-        },
-        {
-            name: "SportOn Product 3",
-            imageUrl: "/images/products/product-3.png",
-            category: "Running",
-            price: 2000,
-            stock: 20
-        },
-        {
-            name: "SportOn Product 4",
-            imageUrl: "/images/products/product-4.png",
-            category: "Running",
-            price: 2000,
-            stock: 20
-        },
-        {
-            name: "SportOn Product 5",
-            imageUrl: "/images/products/product-5.png",
-            category: "Running",
-            price: 2000,
-            stock: 20
-        }
-    ]
+const ProductTable = ({ products, onDelete, onEdit }: TProductTableProps) => {
+
     return (
         <div className="bg-white rounded-xl border border-gray-200">
             <table className="w-full text-left border-collapse">
@@ -56,17 +27,18 @@ const ProductTable = () => {
                 </thead>
                 <tbody>
                     {
-                        datas.map((product, index) => (
+                        products.map((product, index) => (
                             <tr key={index} className="border-b border-gray-200 last:border-b-0">
                                 <td className="px-6 py-4 font-medium">
                                     <div className="flex gap-2 items-center">
                                         <div className="aspect-square bg-gray-100 rounded-md ">
                                             <Image
-                                                src={product.imageUrl}
+                                                src={getImageUrl(product.imageUrl)}
                                                 alt={product.name}
                                                 height="52"
                                                 width="52"
                                                 className="aspect-square object-contain"
+                                                unoptimized={true}
                                             ></Image>
                                         </div>
                                         <span>{product.name}</span>
@@ -74,18 +46,22 @@ const ProductTable = () => {
                                 </td>
                                 <td className="px-6 py-4 font-medium">
                                     <div className="py-1 w-fit bg-gray-200 rounded-md px-2">
-                                        {product.category}
+                                        {product.category.name}
                                     </div>
                                 </td>
                                 <td className="px-6 py-4 font-medium">{PriceFormatter(product.price)}</td>
                                 <td className="px-6 py-4 font-medium">{product.stock} Units</td>
                                 <td className="px-6 py-8 items-center flex h-full">
                                     <div className="flex gap-3">
-                                        <button>
-                                            <FiEdit2 className="cursor-pointer" size={20} />
+                                        <button
+                                            className="cursor-pointer"
+                                            onClick={() => onEdit?.(product)}>
+                                            <FiEdit2 size={20} />
                                         </button>
-                                        <button>
-                                            <FiTrash2 className="cursor-pointer" size={20} />
+                                        <button
+                                            className="cursor-pointer"
+                                            onClick={() => onDelete?.(product._id)}>
+                                            <FiTrash2 size={20} />
                                         </button>
                                     </div>
                                 </td>
