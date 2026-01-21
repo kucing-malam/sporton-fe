e6107dfefc9410b2fca20fd7019293f4ab494755
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 22:59:15 +0700

    feat: add login page

--- app/(auth)/admin/login/page.tsx
+++ app/(auth)/admin/login/page.tsx
@@ -1,11 +1,41 @@
 "use client";
 
 import Button from "@/app/(landing)/components/ui/button"
+import { login } from "@/app/services/auth.services";
 import Image from "next/image"
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const LoginPage = () => {
-    const {push} = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (token) {
+            router.push("/admin/products")
+        }
+    }, [router])
+
+    const handleLogin = async () => {
+        setIsLoading(true);
+        try {
+            const res = await login({ email, password })
+            if (res.token) {
+                router.push("/admin/products")
+            }
+        } catch (error: any) {
+            setErrorMessage(error.message || "Something went wrong, please try again later.");
+            console.log(errorMessage)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <main className="bg-[#F7F9FA] w-full h-screen flex justify-center items-center">
             <div className="w-136 bg-white rounded-xl border-t-6 border-primary p-12 filter-backdrop">
@@ -17,6 +47,11 @@ const LoginPage = () => {
                     className="mx-auto mb-5"
                 />
                 <p className="opacity-50 text-sm text-center mb-8">Enter your credential to access the dashboard</p>
+                {
+                    errorMessage && (<div className="my-3 py-1 px-3 bg-primary-light border-primary border rounded-md text-primary text-sm">
+                        {errorMessage}
+                    </div>)
+                }
                 <div className="flex-col flex gap-4 w-full input-group-admin">
                     <div>
                         <label htmlFor="email">Email</label>
@@ -26,6 +61,8 @@ const LoginPage = () => {
                             id="email"
                             placeholder="Email"
                             className="rounded-lg!"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="mb-12">
@@ -36,10 +73,14 @@ const LoginPage = () => {
                             id="password"
                             placeholder="**********"
                             className="rounded-lg!"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                 </div>
-                <Button onClick={() => push("/admin/products")} className="rounded-lg! w-full mb-10">Signup</Button>
+                <Button onClick={handleLogin} className="rounded-lg! w-full mb-10">
+                    {isLoading ? "Signing in ...." : "Sign In"}
+                </Button>
             </div>
         </main>
     )
