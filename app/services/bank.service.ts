3a98545b1551bebcf459ac7e39a9de3bbf9b29c4
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 23:02:48 +0700

    feat: create service for bank

--- app/services/bank.service.ts
+++ app/services/bank.service.ts
@@ -1,6 +1,37 @@
-import { fetchAPI } from '../lib/api';
+import { fetchAPI, getAuthHeader } from '../lib/api';
 import { Bank } from '../types/index';
 
 export const getAllBank = async (): Promise<Bank[]> => {
     return await fetchAPI<Bank[]>('/banks');
+}
+
+export const createBankOption = async (data: Partial<Bank>): Promise<Bank> => {
+    return await fetchAPI<Bank>('/banks', {
+        body: JSON.stringify(data),
+        method: "POST",
+        headers: {
+            ...getAuthHeader(),
+            "content-type": "application/json"
+        }
+    });
+}
+
+export const updateBankOption = async (id: string, data: Partial<Bank>): Promise<Bank> => {
+    return await fetchAPI<Bank>(`/banks/${id}`, {
+        body: JSON.stringify(data),
+        method: "PUT",
+        headers: {
+            ...getAuthHeader(),
+            "content-type": "application/json"
+        }
+    });
+}
+
+export const deleteBankOption = async (id: string): Promise<void> => {
+    return await fetchAPI<void>(`/banks/${id}`, {
+        headers: {
+            ...getAuthHeader()
+        },
+        method: "DELETE"
+    });
 }
