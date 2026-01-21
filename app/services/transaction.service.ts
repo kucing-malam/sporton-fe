442218c2cccec50757e9be4d220840e9c937ee7e
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 23:03:25 +0700

    feat: create service for transaction

--- app/services/transaction.service.ts
+++ app/services/transaction.service.ts
@@ -1,4 +1,4 @@
-import { fetchAPI } from '../lib/api'
+import { fetchAPI, getAuthHeader } from '../lib/api'
 import { Transaction } from '../types/index'
 
 export const transactionCheckout = async (form: FormData): Promise<Transaction> => {
@@ -10,4 +10,31 @@ export const transactionCheckout = async (form: FormData): Promise<Transaction> 
 
 export const getTransactionByID = async (id: string): Promise<Transaction> => {
     return await fetchAPI<Transaction>(`/transactions/${id}`);
+}
+
+export const getAllTransaction = async (): Promise<Transaction[]> => {
+    return await fetchAPI<Transaction[]>(`/transactions`, {
+        headers: {
+            ...getAuthHeader()
+        }
+    });
+}
+
+export const updateTransaction = async (id: string, data: FormData): Promise<Transaction[]> => {
+    return await fetchAPI<Transaction[]>(`/transactions/${id}`, {
+        method: "PUT",
+        body: data,
+        headers: {
+            ...getAuthHeader()
+        }
+    })
+}
+
+export const deleteTransactions = async (id: string): Promise<void> => {
+    return await fetchAPI<void>(`/transactions/${id}`, {
+        method: "DELETE",
+        headers: {
+            ...getAuthHeader()
+        }
+    });
 }
