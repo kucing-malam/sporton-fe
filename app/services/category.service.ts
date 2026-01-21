61424d4eea80c0ac3e3a5582840159b3695e4792
ferrypradana07 <ferrykun500@gmail.com>
2026-01-21 23:03:00 +0700

    feat: create service for category

--- app/services/category.service.ts
+++ app/services/category.service.ts
@@ -1,6 +1,35 @@
-import { fetchAPI } from '../lib/api'
+import { fetchAPI, getAuthHeader } from '../lib/api'
 import { Category } from '../types/index'
 
 export const getAllCategories = async (): Promise<Category[]> => {
     return await fetchAPI<Category[]>('/categories');
+}
+
+export const createCategory = async (data: FormData): Promise<Category> => {
+    return await fetchAPI<Category>('/categories', {
+        body: data,
+        method: "POST",
+        headers: {
+            ...getAuthHeader()
+        }
+    });
+}
+
+export const updateCategory = async (id: string, data: FormData): Promise<Category> => {
+    return await fetchAPI<Category>(`/categories/${id}`, {
+        body: data,
+        method: "PUT",
+        headers: {
+            ...getAuthHeader()
+        }
+    });
+}
+
+export const deleteCategory = async (id: string): Promise<void> => {
+    return await fetchAPI<void>(`/categories/${id}`, {
+        headers: {
+            ...getAuthHeader()
+        },
+        method: "DELETE"
+    });
 }
